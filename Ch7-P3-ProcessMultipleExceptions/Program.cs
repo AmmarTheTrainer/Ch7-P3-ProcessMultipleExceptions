d55c09248b458b7a510b2a4de3f1b34b96111b1e
Ammar Innowi <ammar.shaukat@innowi.com>
2018-08-10 16:12:57 +0500

    1st commit from head office

---
+++ Ch7-P3-ProcessMultipleExceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch7_P3_ProcessMultipleExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Processing Multiple Exceptions... ");
+
+            Console.ReadLine();
+        }
+    }
+}
